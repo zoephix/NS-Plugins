5047de27a2282dbb46b666f049cdb916ed58be8a
Zep Heijmans <heijmanszep@gmail.com>
2020-07-20 16:37:59 +0200

    Upload spelling plugin

---
+++ spelling/sh_plugin.lua
@@ -0,0 +1,5 @@
+PLUGIN.name = "Spelling"
+PLUGIN.desc = "This plugin makes proper sentences. Converts to capitals in start, and adds periods at the end."
+PLUGIN.author = "Zoephix"
+
+nut.util.includeDir("libs")
