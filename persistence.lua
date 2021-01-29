90ef58a3e4826982b823957282607971b9e3a0d3
Zoephix <heijmanszep2@gmail.com>
2021-01-29 17:16:31 +0100

    Create persistence plugin

---
+++ persistence.lua
@@ -0,0 +1,66 @@
+--[[
+	This script is part of Black Mesa Roleplay schema by Zoephix and
+	exclusively made for LimeFruit (limefruit.net)
+
+	© Copyright 2020: Zoephix. do not share, use, re-distribute or modify
+	without written permission from Zoephix.
+--]]
+
+PLUGIN.name = "Persistence"
+PLUGIN.desc = "Saves persisted props and entities through restarts."
+PLUGIN.author = "Zoephix"
+
+if ( SERVER ) then
+	function PLUGIN:SaveData()
+		local data = {}
+
+		for k, v in pairs(ents.GetAll()) do
+			if (v:GetPersistent()) then
+				local UID = #data + 1
+
+				data[UID] = {
+					pos = v:GetPos(),
+					angles = v:GetAngles(),
+					model = v:GetModel(),
+					entity = v:GetClass(),
+					skin = v:GetSkin(),
+					color = v:GetColor(),
+					material = v:GetMaterial()
+				}
+
+				-- save the motion state if the entity has one
+				if (IsValid(v:GetPhysicsObject())) then
+					data[UID].motion = v:GetPhysicsObject():IsMotionEnabled()
+				end
+			end
+		end
+
+		self:setData(data)
+	end
+
+	function PLUGIN:LoadData()
+		for k, v in pairs(self:getData() or {}) do
+			-- to make sure we don't duplicate the entities
+			for _, entity in pairs(ents.FindInSphere(v.pos, 5)) do
+				if (entity:GetClass() == v.entity) then
+					entity:Remove()
+				end
+			end
+
+			-- spawn the entity
+			local entity = ents.Create(v.entity)
+			entity:SetPos(v.pos)
+			entity:SetAngles(v.angles)
+			entity:SetModel(v.model)
+			entity:SetSkin(v.skin)
+			entity:SetColor(v.color)
+			entity:SetMaterial(v.material)
+			entity:Spawn()
+			entity:Activate()
+
+			if (IsValid(entity:GetPhysicsObject())) then
+				entity:GetPhysicsObject():EnableMotion(v.motion)
+			end
+		end
+	end
+end
