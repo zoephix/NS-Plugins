91cadbfad5bd6a79920df761d487fd9d0483e132
Zoephix <heijmanszep2@gmail.com>
2021-01-29 17:17:33 +0100

    Delete spelling directory

--- spelling/sh_plugin.lua
+++
@@ -1,5 +0,0 @@
-PLUGIN.name = "Spelling"
-PLUGIN.desc = "This plugin makes proper sentences. Converts to capitals in start, and adds periods at the end."
-PLUGIN.author = "Zoephix"
-
-nut.util.includeDir("libs")
